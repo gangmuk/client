792d9b0b3ee5faf43b48b829d35a2430a10f73d9
gangmuk@gmail.com <gangmuk@gmail.com>
2024-11-30 04:30:05 +0000

    plot corecontrast vegeta report

---
+++ run-core-contrast-multiple-times.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+python run_corecontrast.py 30 corecontrast-test-take22
+python run_corecontrast.py 30 corecontrast-test-take23
+python run_corecontrast.py 30 corecontrast-test-take24
+python run_corecontrast.py 30 corecontrast-test-take25
+python run_corecontrast.py 30 corecontrast-test-take26
+python run_corecontrast.py 30 corecontrast-test-take27
+python run_corecontrast.py 30 corecontrast-test-take28
+python run_corecontrast.py 30 corecontrast-test-take29
+python run_corecontrast.py 30 corecontrast-test-take30
